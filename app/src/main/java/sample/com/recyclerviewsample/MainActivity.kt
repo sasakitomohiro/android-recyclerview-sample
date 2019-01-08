5e49f96cb05a38967f62b611f3ab554299f23fd6
t0mat0sk <t0m0.piii.12.07sk@gmail.com>
2019-01-08 22:02:32 +0900

    create OnScrollListener

--- app/src/main/java/sample/com/recyclerviewsample/MainActivity.kt
+++ app/src/main/java/sample/com/recyclerviewsample/MainActivity.kt
@@ -16,6 +16,20 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         val groupAdapter = GroupAdapter<ViewHolder>()
         recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (!recyclerView.canScrollVertically(-1)) {
+                }
+
+                if (!recyclerView.canScrollVertically(1)) {
+                }
+            }
+        })
         recyclerView.adapter = groupAdapter
         val items = ArrayList<RecyclerViewItem>()
         for (i in 1..100) {
