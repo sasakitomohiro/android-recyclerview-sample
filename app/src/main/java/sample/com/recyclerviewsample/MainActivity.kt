24140a038ff5d6bad103d05c7ae6b0667db165e4
thirawat@ookbee.com <ch3a1m0pi2n6g>
2019-10-12 17:15:18 +0700

    - minor refactor.

--- app/src/main/java/sample/com/recyclerviewsample/MainActivity.kt
+++ app/src/main/java/sample/com/recyclerviewsample/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,28 +14,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         val groupAdapter = GroupAdapter<ViewHolder>()
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                super.onScrollStateChanged(recyclerView, newState)
-            }
+        recyclerView?.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+                    if (!recyclerView.canScrollVertically(-1)) {
+                    }
 
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-                if (!recyclerView.canScrollVertically(-1)) {
+                    if (!recyclerView.canScrollVertically(1)) {
+                    }
                 }
-
-                if (!recyclerView.canScrollVertically(1)) {
-                }
+            })
+            recyclerView.adapter = groupAdapter
+            val items = ArrayList<RecyclerViewItem>()
+            for (i in 1..100) {
+                items.add(RecyclerViewItem("aaaa"))
             }
-        })
-        recyclerView.adapter = groupAdapter
-        val items = ArrayList<RecyclerViewItem>()
-        for (i in 1..100) {
-            items.add(RecyclerViewItem("aaaa"))
+            groupAdapter.update(items)
         }
-        groupAdapter.update(items)
     }
 }
